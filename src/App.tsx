61ec85ce818828c9243a926b07ec86aee096647b
tigawanna <denniskinuthiawaweru@gmail.com>
2024-09-11 17:47:32 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+ 
+
+  return (
+    <div className="min-h-screen w-full h-full flex justify-center items-center">
+        <div className="text-2xl">uwu</div>
+    </div>
+  )
+}
+
+export default App
